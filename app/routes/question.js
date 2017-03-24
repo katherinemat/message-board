6740a4aa451df810072f47d81fa490457db5d0d0
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-03-24 11:28:18 -0700

    returns user to same question page when they add an answer to that question.

--- app/routes/question.js
+++ app/routes/question.js
@@ -25,7 +25,7 @@ export default Ember.Route.extend({
       newAnswer.save().then(function(){
         return question.save();
       });
-      this.transitionTo('index');
+      this.transitionTo('question', question);
     }
   }
 });
