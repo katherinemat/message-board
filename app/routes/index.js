121972ee837459a37a84920cf38912cf29796e42
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-03-24 10:00:45 -0700

    add model and content on indiv question route.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    // this.store.findAll('article').then(function(results) {
-    //   return results.toArray().reverse();
-    // });
     return this.store.findAll('question');
   },
   actions: {
