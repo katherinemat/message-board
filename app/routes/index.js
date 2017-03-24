884d3d692e82eba66bee84827b309626382d7a2e
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-03-24 09:41:52 -0700

    create index question tile to display each question on index page.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // model() {
-  //   // this.store.findAll('article').then(function(results) {
-  //   //   return results.toArray().reverse();
-  //   // });
-  //   return this.store.findAll('article');
-  // }
+  model() {
+    // this.store.findAll('article').then(function(results) {
+    //   return results.toArray().reverse();
+    // });
+    return this.store.findAll('question');
+  },
   actions: {
     saveQuestion(params) {
       var newQuestion = this.store.createRecord('question', params);
